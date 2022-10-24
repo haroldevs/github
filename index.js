da3755a93f2ec9adb38dd3d9b15ac1ba55940e5a
haroldevs <haroldthomasg18@gmail.com>
2022-10-24 12:17:07 -0500

    descarga de datos

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// const url=document.querySelector('.image-container')
-
-
-// const getGithub=async function(){
-//     const respuesta=await fetch("https://api.github.com/users/haroldevs")
-//     const data= await respuesta.json()
-
-//     document.getElementsByClassName(".image-container").innerHTML= `
-//     <img src="${data.avatar_url}"></img>
-//     `
-    
-//     //listProfile(data.results)
-//     console.log(data)
-// }
-// const url= "https://api.github.com/users/haroldevs"
-
-// fetch(url).then(response=>response.json()).then(data=>{
-
-//     let imagen=document.getElementsById("imagen")
-//     imagen.innerHTML=`
-//     <p> sdfsdf</p>
-//     <img src="${data.avatar_url}">abc</img>
-//     `
-
-//     console.log(data)
-// }).catch(e=>console.log(err))
 
 document.getElementById('btn-search').addEventListener('click',showGithubUser)
 
@@ -33,18 +7,53 @@ function showGithubUser(){
     let url= 'https://api.github.com/users/'+user
     fetch(url).then(res=>res.json()).then(data=>{
         if(data.message){
-            document.getElementById('container').innerHTML=`
-                <div class="not-found">
-                    <img width="100" src="https://avatars.githubusercontent.com/u/583231?v=4" alt="" />
-                    <h2>No existe el usuario</h2>
-                </div>
-            `
-            return
+            alert('No existe el usuario '+user)
 
         }else{
             console.log(data)
             document.getElementById('imagen').innerHTML=`
                 <img width="100" src="${data.avatar_url}"></img>
+            `
+            document.getElementById('name').innerHTML=`
+            <div>${data.name}</div>
+            `
+
+            const dias=data.created_at
+            const fecha=dias.slice(0, -10)
+
+            const fechaFormato = new Date(fecha)
+            const opciones = {  month: 'short',year: 'numeric', day: 'numeric' }
+            const fechaCreada=fechaFormato.toLocaleDateString('es-PE', opciones)
+
+            document.getElementById('created-at').innerHTML=`
+                <div class="joined">Joined ${fechaCreada}</div>
+            `
+            document.getElementById('login').innerHTML=`
+            <div>@${data.login}</div>
+            `
+            document.getElementById('bio').innerHTML=`
+            <div>@${data.bio}</div>
+            `
+            document.getElementById('public_repos').innerHTML=`
+            <div>${data.public_repos}</div>
+            `
+            document.getElementById('followers').innerHTML=`
+            <div>${data.followers}</div>
+            `
+            document.getElementById('following').innerHTML=`
+            <div>${data.following}</div>
+            `
+            document.getElementById('location').innerHTML=`
+            <div><img width="15" src="./image/pin.png" alt="" />&nbsp;${data.location}</div>
+            `
+            document.getElementById('blog').innerHTML=`
+            <div><img width="15" src="./image/link.png" alt="" />&nbsp;${data.blog}</div>
+            `
+            document.getElementById('twitter_username').innerHTML=`
+            <div><img width="15" src="./image/twitter.png" alt="" />&nbsp;${data.twitter_username}</div>
+            `
+            document.getElementById('company').innerHTML=`
+            <div><img width="15" src="./image/hotel.png" alt="" />&nbsp;${data.company}</div>
             `
         }
         
